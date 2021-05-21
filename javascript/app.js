8418d9178f9c9262346883e70715750cb12a3297
Zack agba <zackagba7@gmail.com>
2021-05-21 19:40:53 +0100

    🗑 removed a few files

--- javascript/app.js
+++ javascript/app.js
@@ -1,181 +1,54 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach((el) => {
-      el.addEventListener("click", () => {
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
-    });
-  }
+document.addEventListener("DOMContentLoaded", function () {
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function (el) {
+            el.addEventListener("click", function () {
+                // Get the target from the "data-target" attribute
+                var target = el.dataset.target;
+                var $target = document.getElementById(target);
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle("is-active");
+                $target.classList.toggle("is-active");
+            });
+        });
+    }
 });
-
-const fileInput = document.querySelector("#file-js-example input[type=file]");
-fileInput.onchange = () => {
-  if (fileInput.files.length > 0) {
-    const fileName = document.querySelector("#file-js-example .file-name");
-    fileName.textContent = fileInput.files[0].name;
-  }
+var fileInput = document.querySelector("#file-js-example input[type=file]");
+fileInput.onchange = function () {
+    if (fileInput.files.length > 0) {
+        var fileName = document.querySelector("#file-js-example .file-name");
+        fileName.textContent = fileInput.files[0].name;
+    }
 };
-
-const imgDiv = document.querySelector(".profile-pic-div");
-const img = document.querySelector("#photo");
-const file = document.querySelector("#file");
-const uploadBtn = document.querySelector("#uploadBtn");
-
-imgDiv.addEventListener("mouseenter", function () {
-  uploadBtn.style.display = "block";
-});
-
-imgDiv.addEventListener("mouseleave", function () {
-  uploadBtn.style.display = "none";
-});
-
-//when we choose a Photo to upload
-
-file.addEventListener("change", function () {
-  //this refers to a file
-  const choosedFile = this.files[0];
-
-  if (choosedFile) {
-    const reader = new FileReader(); //FileReader is a predefined function of JS
-
-    reader.addEventListener("load", function () {
-      img.setAttribute("src", reader.result);
-    });
-
-    reader.readAsDataURL(choosedFile);
-  }
-});
-
-const keyChangeBtn = document.querySelector("#changeKey");
-const readBtn = document.querySelector("#speak");
-const code = document.querySelector("#code");
-const input = document.querySelector("#uinput");
-const submitBtn = document.querySelector("#btn");
-
-//now if user click on ckange key button then
-
-keyChangeBtn.addEventListener("click", function () {
-  code.textContent = createCaptcha();
-});
-
-//also if window is refresed then
-
-window.addEventListener("load", function () {
-  code.textContent = createCaptcha();
-});
-
-//now lets create the function
-
-function createCaptcha() {
-  let letters = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
-
-  let a = letters[Math.floor(Math.random() * letters.length)];
-  let b = letters[Math.floor(Math.random() * letters.length)];
-  let c = letters[Math.floor(Math.random() * letters.length)];
-  let d = letters[Math.floor(Math.random() * letters.length)];
-  let e = letters[Math.floor(Math.random() * letters.length)];
-  let f = letters[Math.floor(Math.random() * letters.length)];
-  let g = letters[Math.floor(Math.random() * letters.length)];
-
-  let code = a + b + c + d + e + f + g;
-
-  return code;
+var keys = "abcdefghijklmnopqrstubwsyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+var code = '';
+function getCode(button) {
+    code = generateCode(8);
+    document.getElementById('code').innerHTML = "<center><label style='font-size:35px; margin-top:30px;'>" + code + "</label></center>";
+    document.getElementById('button').removeAttribute('disabled');
+    button.setAttribute('disabled', 'disabled');
 }
-
-submitBtn.addEventListener("click", function () {
-  let inpValue = input.value;
-  if (inpValue == "") {
-    alert("Please enetr the key");
-  } else if (inpValue === code.textContent) {
-    alert("Valid Code Redirect Where You Want");
-  } else {
-    alert("Invalid Code! Try Again");
-  }
-});
-
-readBtn.addEventListener("click", function () {
-  let text = code.textContent;
-  responsiveVoice.speak(text);
-});
-
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+function generateCode(len) {
+    code = '';
+    for (i = 0; i < len; i++) {
+        code += keys.charAt(Math.floor(Math.random() * keys.length));
+    }
+    return code;
+}
+function checkCode(button) {
+    var val = document.getElementById('ccode').value;
+    if (val == "") {
+        document.getElementById('result').innerHTML = "<center class='text-danger'>Fill up the form first!</center>";
+    }
+    else {
+        if (code == val) {
+            document.getElementById('result').innerHTML = "<center class='text-success'>The code can be use</center>";
+        }
+        else {
+            document.getElementById('result').innerHTML = "<center class='text-danger'>Invalid code<center>";
+        }
+    }
 }
